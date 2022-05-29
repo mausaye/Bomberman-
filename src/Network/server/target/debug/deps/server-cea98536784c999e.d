588b2077fbf1c725e0f82197a2e583b5625a5a1c
Robbie Hammond <robbiehammond3@gmail.com>
2022-05-29 10:55:01 -0400

    remove compiled rust

--- src/Network/server/target/debug/deps/server-cea98536784c999e.d
+++
@@ -1,5 +0,0 @@
-/Users/roberthammond/Documents/Code/Bomberman2.0/src/Network/server/target/debug/deps/server-cea98536784c999e: src/main.rs
-
-/Users/roberthammond/Documents/Code/Bomberman2.0/src/Network/server/target/debug/deps/server-cea98536784c999e.d: src/main.rs
-
-src/main.rs:
--- src/Network/server/target/debug/server.d
+++
@@ -1,1 +0,0 @@
-/Users/roberthammond/Documents/Code/Bomberman2.0/src/Network/server/target/debug/server: /Users/roberthammond/Documents/Code/Bomberman2.0/src/Network/server/src/main.rs
